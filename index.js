a31285ec1a9e93d330757c476b6cd6dfa43154c3
majaliju <majalijunyc@gmail.com>
2022-02-01 17:40:32 -0500

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// re-worked version of a method I found
+
+
+function titleCased(){
+  return tutorials.map(sentence => {
+    // breaks each string in the array into a new array, which contains each word in the sentence
+    const words = sentence.split(" ");
+    // breaks down each string in that new array, into words
+    // each word is capitalized only at the first letter, then the rest of the word body is sliced back on
+    const newlyCappedWord = words.map(element => element[0].toUpperCase() + element.slice(1));
+    // the newly capitalized words are rejoined back into a string, to undo our original split()
+    return newlyCappedWord.join(" ")
+})}
